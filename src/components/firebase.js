4669714a0744906a69a83dd470fd6341549d8ea3
tsahiBarshevsky <tsahi.13@gmail.com>
2020-12-14 17:08:49 +0200

    Basic authentication system with pages

---
+++ src/components/firebase.js
@@ -0,0 +1,74 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVgBNDlARe8CY5YRXP_vJXcrcMVVoMV00",
+    authDomain: "portfolio-plus-3caf2.firebaseapp.com",
+    projectId: "portfolio-plus-3caf2",
+    storageBucket: "portfolio-plus-3caf2.appspot.com",
+    messagingSenderId: "477542785620",
+    appId: "1:477542785620:web:665a0eb78cf26635f543c1"
+};
+  
+
+class Firebase
+{
+    constructor()
+    {
+        app.initializeApp(firebaseConfig);
+        this.auth = app.auth();
+        this.db = app.firestore();
+    }
+
+    login(email, password)
+    {
+        return this.auth.signInWithEmailAndPassword(email, password);
+    }
+
+    logout()
+    {
+        return this.auth.signOut();
+    }
+
+    async register(name, email, password)
+    {
+        await this.auth.createUserWithEmailAndPassword(email, password);
+        return this.auth.currentUser.updateProfile({
+            displayName: name
+        });
+    }
+
+    addQuote(quote)
+    {
+        if (!this.auth.currentUser)
+            return alert("Not authorized");
+        return this.db.doc(`users_codedamn_video/${this.auth.currentUser.uid}`).set({
+            quote: quote,
+            user: this.auth.currentUser.displayName
+        });
+    }
+
+    isInitialized()
+    {
+        return new Promise(resolve => {
+            this.auth.onAuthStateChanged(resolve);
+        });
+    }
+
+    getCurrentUsername()
+    {
+        return this.auth.currentUser && this.auth.currentUser.displayName;
+    }
+
+    async getCurrentUserQuote()
+    {
+        if (this.auth.currentUser)
+        {
+            const quote = await this.db.doc(`users_codedamn_video/${this.auth.currentUser.uid}`).get()
+            return quote.get('quote');
+        }
+    }
+}
+
+export default new Firebase();
