7f5dd28ed5e017c47e5155129bc726ed1148876a
tsahiBarshevsky <tsahi.13@gmail.com>
2020-12-21 20:56:45 +0200

    Alert before delete projcet; Success snackbar

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createMuiTheme, makeStyles, MuiThemeProvider } from '@material-ui/core/styles';
-import { Fab, Paper, Typography } from '@material-ui/core';
+import { Button, DialogActions, Fab, Paper, Typography, Snackbar } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import firebase from '../firebase';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import WarningIcon from '@material-ui/icons/Warning';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles(theme => ({
     main: 
@@ -45,18 +50,36 @@ const theme = createMuiTheme({
 		allVariants:
 		{
 			fontFamily: `"Andika New Basic", sans-serif`,
-			padding: '10px'
 		},
-		subtitle1:
-		{
-			fontSize: '20px'
-		}
 	}
 });
 
 function ProjectCard(props) 
 {
-    const classes = useStyles();
+	const classes = useStyles();
+	const [open, setOpen] = useState(false);
+	const [openSuccess, setOpenSuccess] = useState('');
+
+	const Alert = (props) =>
+    {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+	}
+
+	const handleOpen = () =>
+	{
+		setOpen(true);
+	}
+
+	const handleClose = () =>
+	{
+		setOpen(false);
+	}
+
+	const closeSnackbar = () =>
+	{
+		setOpenSuccess(false);
+	}
+
     return (
 		<Paper className={classes.paper}>
 			<MuiThemeProvider theme={theme}>
@@ -71,10 +94,47 @@ function ProjectCard(props)
 				<Fab className={classes.fab} color="primary" size="small">
 					<EditIcon />
 				</Fab>
-				<Fab className={classes.fab} color="primary" size="small" onClick={deleteProject}>
+				<Fab className={classes.fab} color="primary" size="small" onClick={handleOpen}>
 					<DeleteIcon />
 				</Fab>
 			</div>
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+				style={{cursor: "default"}}>
+					<DialogTitle >
+						<div style={{display: 'flex', flexDirection: 'row'}}>
+							<WarningIcon style={{fontSize: '30px'}} />
+							<MuiThemeProvider theme={theme}>
+								<Typography component="h1" variant="h5">
+									{`Delete project`}
+								</Typography>
+							</MuiThemeProvider>
+						</div>
+					</DialogTitle>
+					<DialogContent>
+						<MuiThemeProvider theme={theme}>
+							<Typography variant="h6">
+								{`Wait! Are you sure you want to delete ${props.title}?`}
+							</Typography>
+						</MuiThemeProvider>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose}>Cancel</Button>
+						<Button onClick={deleteProject}>Delete</Button>
+					</DialogActions>
+			</Dialog>
+			<Snackbar open={openSuccess} autoHideDuration={3500} onClose={closeSnackbar}>
+				<Alert onClose={closeSnackbar} severity="success">
+					<MuiThemeProvider theme={theme}>
+						<Typography align="center" variant="subtitle1">
+							{`Project deleted successfully!`}
+						</Typography>
+					</MuiThemeProvider>
+				</Alert>
+			</Snackbar>
 		</Paper>
 	);
 	
@@ -82,7 +142,9 @@ function ProjectCard(props)
 	{
 		try 
 		{
+			setOpen(false);
 			await firebase.deleteProject(props.title);
+			setOpenSuccess(true);
 		} 
 		catch (error)
 		{
